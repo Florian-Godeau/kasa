a04688db2329a3807fd35465189d4b01fe3c6a69
Florian-Godeau <flgodeau@gmail.com>
2023-09-06 14:54:50 +0200

    Premier commit

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // BrowserRouter utilise la gestion des URL du navigateur
+import Home from "./pages/home";
+import About from "./pages/about";
+import Accomodations from "./pages/accomodations";
+import Error from "./pages/error";
+
+function Router() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/accomodation/:id" element={<Accomodations />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
+
+export default Router;
