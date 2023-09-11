f9dd8103c8f90bc750989d194055e3700e2c84f5
Florian-Godeau <flgodeau@gmail.com>
2023-09-11 17:19:01 +0200

    finalisation page home desktop et début responsive

---
+++ src/components/gallery/gallery.jsx
@@ -0,0 +1,20 @@
+import "./gallery.scss";
+import datas from "../../datas/accommodations.json";
+import Card from "../cards/cards";
+
+export default function Gallery() {
+    return (
+        <main className="gallery">
+            {datas.map(data => {
+                return (
+                    <Card
+                        key={data.id}
+                        id={data.id}
+                        title={data.title}
+                        cover={data.cover}
+                    />
+                )
+            })}
+        </main>
+    )
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,15 @@
 import Header from "../../components/header/header";
 import Banner from "../../components/banner/banner";
+import Gallery from "../../components/gallery/gallery";
+import Footer from "../../components/footer/footer";
 
 export default function Home() {
   return (
     <div className="home">
       <Header />
       <Banner />
+      <Gallery />
+      <Footer />
     </div>
   );
 }
