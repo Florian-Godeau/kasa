abea016611ecc9cd606461bc14ad6d403b23a64e
Florian-Godeau <flgodeau@gmail.com>
2023-09-13 14:31:46 +0200

    finalisation page d'erreur desktop et responsive

---
+++ src/pages/error/error.jsx
@@ -0,0 +1,18 @@
+import "./error.scss"
+import Header from "../../components/header/header";
+import { Link } from "react-router-dom";
+import Footer from "../../components/footer/footer";
+
+export default function Error() {
+    return (
+        <div className="error">
+            <Header />
+            <div className="error__content">
+                <h1 className="error__content__title">404</h1>
+                <p className="error__content__txt">Oups! La page que vous demandez n'existe pas.</p>
+                <Link to="/" className="error__content__link">Retourner sur la page d'accueil</Link>
+            </div>
+            <Footer />
+        </div>
+    )
+}
